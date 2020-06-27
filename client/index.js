04af4aa5c960f10df46112236e5605d144fe4985
Cesar Nina <c3sar207@gmail.com>
2020-06-27 01:12:48 -0400

    created webpack with babel to display react onto the browser

--- client/index.js
+++ client/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Main from './Main'
-import { Provider } from "react-redux"
-import store from "./store"
+import Main from './Components/Main'
+// import { Provider } from "react-redux"
+// import store from "./store"
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Main />
-  </Provider>,
+  // <Provider store={store}>
+    <Main />,
+  // </Provider>,
   document.getElementById('app')
 )
+
