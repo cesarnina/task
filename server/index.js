61984a18acafac606e5cf57c25b3829e995c2364
Cesar Nina <c3sar207@gmail.com>
2020-07-23 01:30:01 -0400

    set up the app.use and middleware

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+module.exports = app
+
+// logging middleware
+app.use(morgan('dev'))
+
+// Body parsing middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// Static file-serving middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// Routes that will be accessed via AJAX should be prepended with
+// /api so they are isolated from our GET /* wildcard.
+app.use('/api', require('./api'))
+
+// Send index.html for any other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
