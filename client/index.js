b203f2913dbce63fa28fc8ff4eb5246148bdb779
Cesar Nina <c3sar207@gmail.com>
2020-07-31 23:36:22 -0400

    fixed a bug with the path

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './Components/Main';
+import Main from './components/Main';
 import { Provider } from "react-redux";
 import store from './store/rootStore';
 
